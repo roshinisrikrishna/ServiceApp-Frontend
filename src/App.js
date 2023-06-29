a0d27fca88d6f4e4e033b8eeb8db13befeb5f9a0
Roshini R <roshinisrikrishna@gmail.com>
2023-06-29 22:48:18 +0530

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './pages/Home';
+import AddUser from './pages/AddUser';
+import EditUser from './pages/EditUser';
+import View from './pages/View';
+import TravelLog from './pages/TravelLog';
+import Login from './pages/Login';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ToastContainer position="top-center"/>
+    <Routes>
+    <Route exact path='/' element={<Login/>}/>
+    <Route path='/users' element={<Home/>} />
+    <Route path='/users/create' element={<AddUser/>}/>
+    <Route path='/user/update/:id' element={<EditUser/>}/>
+    <Route path='/user/get/:id' element={<View/>}/>
+    <Route path='/travel/:id' element={<TravelLog/>}/>
+    </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
